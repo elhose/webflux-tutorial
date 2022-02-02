1c6381ee0e066b07f22fc62add0498fac51ad103
elhose <elhose>
2022-02-02 13:14:55 +0100

    Created dummy math service in NON-blocking fashion

---
+++ webflux-demo/src/main/java/com/js/webfluxdemo/controller/ReactiveMathController.java
@@ -0,0 +1,29 @@
+package com.js.webfluxdemo.controller;
+
+import com.js.webfluxdemo.dto.Response;
+import com.js.webfluxdemo.service.ReactiveMathService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("reactive-math")
+@RequiredArgsConstructor
+public class ReactiveMathController {
+    private final ReactiveMathService mathService;
+
+    @GetMapping("square/{number}")
+    public Mono<Response> getSquare(@PathVariable int number) {
+        return mathService.getSquare(number);
+    }
+
+    @GetMapping("table/{number}")
+    public Flux<Response> getMultiplicationTable(@PathVariable int number) {
+        return mathService.getMultiplicationTable(number);
+    }
+
+}
---
+++ webflux-demo/src/main/java/com/js/webfluxdemo/service/ReactiveMathService.java
@@ -0,0 +1,30 @@
+package com.js.webfluxdemo.service;
+
+import com.js.webfluxdemo.dto.Response;
+import com.js.webfluxdemo.util.SleepUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Service
+public class ReactiveMathService {
+
+    public Mono<Response> getSquare(int number) {
+        return Mono.fromSupplier(() -> number * number)
+                   .map(Response::new);
+    }
+
+    public Flux<Response> getMultiplicationTable(int number) {
+        return Flux.range(1, 10)
+                   .doOnNext(this::simulateLongProcess)
+                   .map(i -> i * number)
+                   .map(Response::new);
+    }
+
+    private void simulateLongProcess(int currentNumber) {
+        log.info("REACTIVE MathService currently working on -> " + currentNumber);
+        SleepUtil.sleepSeconds(1);
+    }
+}
