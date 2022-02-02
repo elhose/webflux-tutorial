7280fcf0d5d5464337b767b3a018903821bd5fbd
elhose <elhose>
2022-02-02 17:01:48 +0100

    Routers can have additional predicates on paths

--- webflux-demo/src/main/java/com/js/webfluxdemo/config/RouterConfig.java
+++ webflux-demo/src/main/java/com/js/webfluxdemo/config/RouterConfig.java
@@ -4,6 +4,7 @@ import com.js.webfluxdemo.exception.DummyException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -17,6 +18,18 @@ public class RouterConfig {
     public RouterFunction<ServerResponse> highLevelRouter() {
         return RouterFunctions.route()
                               .path("router", this::serverResponseRouterFunction)
+                              .path("predicate", this::predicateResponseRouterFunction)
+                              .build();
+    }
+
+    private RouterFunction<ServerResponse> predicateResponseRouterFunction() {
+        return RouterFunctions.route()
+                              .GET("square/{number}", RequestPredicates.path("*/1?"),
+                                   request -> ServerResponse.ok().bodyValue("FIRST CONDITION"))
+                              .GET("square/{number}", RequestPredicates.path("*/0"),
+                                   request -> ServerResponse.ok().bodyValue("ZERO"))
+                              .GET("square/{number}",
+                                   request -> ServerResponse.badRequest().bodyValue("ERROR ON EVEYTHING ELSE"))
                               .build();
     }
 
@@ -27,7 +40,8 @@ public class RouterConfig {
                               .GET("table/{number}/stream", requestHandler::tableHandlerStream)
                               .POST("multiply", requestHandler::multiplyHandler)
                               .GET("square/{number}/error", requestHandler::squareErrorHandler)
-                              .onError(DummyException.class, (e, serverRequest) -> ServerResponse.badRequest().bodyValue(e.getMessage()))
+                              .onError(DummyException.class,
+                                       (e, serverRequest) -> ServerResponse.badRequest().bodyValue(e.getMessage()))
                               .build();
     }
 }
